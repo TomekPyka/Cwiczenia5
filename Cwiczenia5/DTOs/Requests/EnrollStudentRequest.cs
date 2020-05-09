470fea24a1af6ef959319cf138e52728cdfd0be0
TomekPyka <61656933+TomekPyka@users.noreply.github.com>
2020-05-09 19:33:34 +0200

    At 31:27

--- Cwiczenia5/DTOs/Requests/EnrollStudentRequest.cs
+++ Cwiczenia5/DTOs/Requests/EnrollStudentRequest.cs
@@ -6,25 +6,28 @@ using System.Threading.Tasks;
 
 namespace Cwiczenia5.DTOs.Requests
 {
-    public class EnrollStudentRequest 
+    public class EnrollStudentRequest
     {
-        [RegularExpression("^short[0-9]+$")]
-        public string IndexNumber { get; set; }
+  
+            [RegularExpression("^short[0-9]+$")]
+            public string IndexNumber { get; set; }
 
-        //[Range(1, 10)]
-        //[Min(10)]
-        //public int Age { get; set; }
+            //[Range(1, 10)]
+            //[Min(10)]
+            //public int Age { get; set; }
 
-        [Required]
-        [MaxLength(10)] //VARCHAR(10)
-        public string FirstName { get; set; }
+            ////[Required]
+            [MaxLength(10)] //VARCHAR(10)
+            public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(255)] //VARCHAR(255)
-        public string LastName { get; set; }
+            [Required]
+            [MaxLength(255)] //VARCHAR(255)
+            public string LastName { get; set; }
 
-        public DateTime BirthDate { get; set; }
+            public DateTime BirthDate { get; set; }
 
-        public string Studies { get; set; }
+            [Required]
+            public string Studies { get; set; }
+        }
     }
-}
+
